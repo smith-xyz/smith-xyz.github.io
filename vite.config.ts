f260424b0fb0c05485fa8ffc52c96fc2f86fa0e5
smith-xyz <51304449+smith-xyz@users.noreply.github.com>
2024-10-14 22:45:52 -0400

    fix: vite config base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/smith-xyz.github.io",
+  base: "./smith-xyz.github.io",
   plugins: [react()],
 });
